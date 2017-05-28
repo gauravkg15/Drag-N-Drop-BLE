2b4b0c90cdd26ca51aeb8d0fe8fac935a0104e81
Gaurav Gupta <hi.gauravg@gmail.com>
2017-05-28 16:10:02 -0700

    Initial Commit

---
+++ BLE SingletonTests/BLE_SingletonTests.swift
@@ -0,0 +1,36 @@
+//
+//  BLE_SingletonTests.swift
+//  BLE SingletonTests
+//
+//  Created by Gaurav Gupta on 5/28/17.
+//  Copyright © 2017 GauravGupta. All rights reserved.
+//
+
+import XCTest
+@testable import BLE_Singleton
+
+class BLE_SingletonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
